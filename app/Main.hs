2c27b0ef1253c332d041da09c7291c9ef67390c5
Jim Holmstrom <jim.holmstroem@gmail.com>
2016-06-25 19:36:14 -0400

    Starting to work

--- app/Main.hs
+++ app/Main.hs
@@ -3,54 +3,66 @@
 
 module Main where
 
-import Control.Monad
-import Control.Monad.IO.Class
-import Control.Applicative
-import Data.Traversable
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Control.Applicative
+import           Data.Traversable
 
-import GHC.Generics
+import           GHC.Generics
 
-import System.Random
+import           System.Random
 
 import qualified Data.ByteString.Char8 as BSC8
 import qualified Data.ByteString.Lazy.Char8 as BSLC8
+import           Data.Text.Encoding (encodeUtf8, decodeUtf8)
 
-import Data.UUID
-import Data.Time.Clock
-import Data.Time.ISO8601
+import           Data.UUID
+import           Data.Time.Clock
+import           Data.Time.ISO8601
 
-import Data.Aeson
-import Data.Aeson.Types
+import           Data.Aeson
+import           Data.Aeson.Types
 
-import Database.Redis
-import Snap
-import Snap.Util.FileServe
+import           Database.Redis
+import           Snap
+import           Snap.Util.FileServe
 
-import Lib
+import           Lib
+
 
 -- formatISO8601
--- getCurrentTime
 
 
 instance FromJSON UUID where
-    parseJSON value@(Data.Aeson.String content) = case Data.UUID.fromText content of (Just uuid) -> return uuid
-                                                                                     Nothing -> typeMismatch "UUID" value
+    parseJSON value@(Data.Aeson.String content) = case Data.UUID.fromText content of
+        (Just uuid) -> return uuid
+        Nothing -> typeMismatch "UUID" value
     parseJSON invalid = typeMismatch "UUID" invalid
 instance ToJSON UUID where
     toJSON uuid = Data.Aeson.String $ Data.UUID.toText uuid
 
 
-data ChatMessage =
-    ChatMessage { uuid :: UUID, timestamp :: UTCTime, user :: String, content :: String }
+data ChatMessage = ChatMessage { uuid      :: UUID
+                               , timestamp :: UTCTime
+                               , user      :: BSC8.ByteString
+                               , content   :: BSC8.ByteString
+                               }
     deriving (Read, Show, Eq, Generic)
 
 
-mkMessage :: String -> String -> IO ChatMessage
+mkMessage :: BSC8.ByteString -> BSC8.ByteString -> IO ChatMessage
 mkMessage user content = ChatMessage <$> randomIO <*> getCurrentTime <*> return user <*> return content
 
 
 instance FromJSON ChatMessage
 instance ToJSON ChatMessage
+
+
+instance FromJSON BSC8.ByteString where
+    parseJSON value@(Data.Aeson.String content) = return $ encodeUtf8 content
+    parseJSON invalid = typeMismatch "ByteString" invalid
+instance ToJSON BSC8.ByteString where
+    toJSON = Data.Aeson.String . decodeUtf8
 
 
 response :: Int -> Snap ()
@@ -63,10 +75,13 @@ response code = do
 postMessage :: Connection -> Snap ()
 postMessage conn = do
     Just boardId <- getParam "boardId"
-    message <- liftIO $ mkMessage "user" "content"
+    Just user <- getPostParam "user"
+    Just content <- getPostParam "content"
+
+    message <- liftIO $ mkMessage user content
     result <- liftIO $ runRedis conn $ multiExec $ do
         lpush (BSC8.append "messages:" boardId) [Data.UUID.toASCIIBytes $ uuid message]
-        set (BSC8.append "message:" $ Data.UUID.toASCIIBytes $ uuid message) $ BSC8.pack $ show message
+        set (BSC8.append "message:" $ Data.UUID.toASCIIBytes $ uuid message) $ BSLC8.toStrict $ encode message
 
     case result of
         (TxSuccess _) -> response 201
@@ -77,16 +92,21 @@ postMessage conn = do
 getMessages :: Connection -> Snap ()
 getMessages conn = do
     Just boardId <- getParam "boardId"
-    result <- liftIO $ runRedis conn $ multiExec $ lrange (BSC8.append "messages:" boardId) 0 (-1)
+    Right messageIds <- liftIO $ runRedis conn $ lrange (BSC8.append "messages:" boardId) 0 (-1)
+    messages <- liftIO $ runRedis conn $ getMessagFromIds messageIds
 
-    case result of
-        (TxSuccess messageIds) -> do
-            let queries = fmap (\uuid->get $ BSC8.append "message:" uuid) messageIds
-            messages <- liftIO $ runRedis conn $ multiExec $ Data.Traversable.sequence queries
-            writeBS $ BSC8.pack $ show messages
+    mapM_ (writeBS . BSC8.pack . (++ "\n") . show) messages
+        where getMessagFromIds (id:ids) = (:) <$> get (BSC8.append "message:" id)  <*> getMessagFromIds ids
+              getMessagFromIds [] = pure []
 
-        TxAborted -> response 500
-        TxError errorMsg -> response 500
+
+--    case result of
+--        (Right messages) -> return ()
+--    case result of
+--        (TxSuccess messageIds) -> do
+--            let queries = fmap (\uuid->get $ BSC8.append "message:" uuid) messageIds
+--            messages <- liftIO $ runRedis conn $ multiExec $ Data.Traversable.sequence queries
+--            writeBS $ BSC8.pack $ show messages
 
 
 top :: Snap ()
